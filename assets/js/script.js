55f318aaf23d96aed7efe5cb853dff3b2aa00d64
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-07-11 19:41:00 -0400

    added savebutton function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,11 @@ let savedEvents = [];
 let events = {};
 
 const createEvent = (rowId) => {
-    // create text area
     let textAreaEl = $("<textarea>");
     textAreaEl.val("New Event")
 
-    // highlight text box
     textAreaEl.focus();
 
-    // append text area to selected event
     $("#" + rowId).append(textAreaEl);
  };
 
@@ -21,6 +18,41 @@ const createEvent = (rowId) => {
  };
 
  const saveEvent = () => {
-    // save to local storage
+
     localStorage.setItem("events", JSON.stringify(savedEvents));
+ };
+
+ const loadEvents = () => {
+    savedEvents = JSON.parse(localStorage.getItem("events"));
+    if(!savedEvents) {
+        savedEvents = [];
+    }
+    $.each(savedEvents, function(index) {
+        let eventText = savedEvents[index].eventVal;
+        let rowId = savedEvents[index].rowId;
+        let eventEl = $("<p>").text(eventText);
+
+        $(".event#" + rowID).append(eventEl);
+    });
+ };
+
+ const saveButton = rowId => {
+    let eventEl = $("#" + rowId + "textarea");
+    let eventText = eventEl.val();
+
+    let savedEventEl = $("<p>").text(eventText);
+
+    let updatedEvent = eventEl.replaceWith(savedEventEl);
+    let updatedVal = updatedEvent.val();
+    if (updatedVal) {
+        eventObjHandler(updatedVal, rowId);
+
+        for (i=0, i < savedEvents.length; i++) {
+            if(savedEvents[i].rowId === rowId) {
+                savedEvents,splice(i, 1);
+            }
+        }
+    }
+    savedEvents.push(events);
+    saveEvent();
  };
